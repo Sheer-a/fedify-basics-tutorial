2d544a560a4c9628e31afc47b5b0b7f31ad311c1
Sheer <Sheer.Ahmed@live.de>
2024-12-24 18:55:08 +0000

    Initial commit: Projektstruktur und Basisdateien hinzugefügt

---
+++ follow-server/server.ts
@@ -0,0 +1,22 @@
+import { serve } from "@hono/node-server";
+import { createFederation, MemoryKvStore } from "@fedify/fedify";
+import { configure, getConsoleSink } from "@logtape/logtape";
+
+await configure({
+  sinks: { console: getConsoleSink() },
+  filters: {},
+  loggers: [
+    { category: "fedify",  sinks: ["console"], lowestLevel: "info" },
+  ],
+});
+
+const federation = createFederation<void>({
+  kv: new MemoryKvStore(),
+});
+
+serve({
+  port: 8000,
+  fetch(request) {
+    return federation.fetch(request, {contextData: undefined });
+  }
+});
