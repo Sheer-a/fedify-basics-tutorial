bc9af08baa8dd76538d0d7e3cedf5049608a0dde
Sheer <Sheer.Ahmed@live.de>
2024-12-29 16:40:52 +0000

    step 2

--- follow-server/server.ts
+++ follow-server/server.ts
@@ -1,8 +1,11 @@
 import { serve } from "@hono/node-server";
-import { createFederation, MemoryKvStore } from "@fedify/fedify";
+import { behindProxy } from "x-forwarded-fetch";
+import { createFederation, MemoryKvStore, Person } from "@fedify/fedify";
 import { configure, getConsoleSink } from "@logtape/logtape";
+import { AsyncLocalStorage } from "node:async_hooks";
 
 await configure({
+  contextLocalStorage: new AsyncLocalStorage(),
   sinks: { console: getConsoleSink() },
   filters: {},
   loggers: [
@@ -14,9 +17,21 @@ const federation = createFederation<void>({
   kv: new MemoryKvStore(),
 });
 
-serve({
-  port: 8000,
-  fetch(request) {
-    return federation.fetch(request, {contextData: undefined });
-  }
+federation.setActorDispatcher("/users/{identifier}", async (ctx, identifier) => {
+  if (identifier !== "me") return null;  // Other than "me" is not found.
+  return new Person({
+    id: ctx.getActorUri(identifier),
+    name: "Me",  // Display name
+    summary: "This is me!",  // Bio
+    preferredUsername: identifier,  // Bare handle
+    url: new URL("/", ctx.url),
+  });
 });
+
+serve({
+  port: 3000,
+  fetch: behindProxy((request) => federation.fetch(request, { contextData: undefined })),
+});
+
+
+// node --import tsx server.ts
